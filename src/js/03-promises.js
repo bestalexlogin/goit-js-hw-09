c33b25a3a4af8821f8dbc87b2d0a9b6575e5e7a5
bestalexlogin <never.fade.ever@gmail.com>
2023-11-15 13:31:30 +0200

    comm

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.js-form');
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+
+  let {
+    elements: { delay, step, amount },
+  } = evt.currentTarget;
+
+  delay = Number(delay.value);
+  amount = Number(amount.value);
+  step = Number(step.value);
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay);
+    delay += step;
   }
+}
+
+function createPromise(position, delay) {
+  setTimeout(() => {
+    const shouldResolve = Math.random() > 0.3;
+
+    const promise = new Promise((resolve, reject) => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    });
+
+    promise
+      .then(value => Notiflix.Notify.success(value))
+      .catch(error => Notiflix.Notify.failure(error));
+  }, delay);
 }
