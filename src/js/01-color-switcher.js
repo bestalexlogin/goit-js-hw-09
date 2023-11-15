da433ce18a5c83da44af645472793a7e5f65fa8a
bestalexlogin <never.fade.ever@gmail.com>
2023-11-15 13:04:52 +0200

    commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+refs.stopBtn.setAttribute('disabled', 'disabled');
+
+refs.startBtn.addEventListener('click', onStart);
+refs.stopBtn.addEventListener('click', onStop);
+
+let intervalId = 0;
+
+function onStart() {
+  refs.startBtn.setAttribute('disabled', 'disabled');
+  refs.stopBtn.removeAttribute('disabled', 'disabled');
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStop() {
+  clearInterval(intervalId);
+  refs.startBtn.removeAttribute('disabled', 'disabled');
+  refs.stopBtn.setAttribute('disabled', 'disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
